7bb64507d45b464edd456d7d928fb089cfc6ca25
r21238121-netizen <r21238121@gmail.com>
2025-11-22 15:02:42 +0300

    Delete bot directory

--- bot/states.py
+++
@@ -1,8 +0,0 @@
-# bot/states.py
-from aiogram.fsm.state import State, StatesGroup
-
-class MasterApplication(StatesGroup):
-    waiting_for_city = State()
-    waiting_for_services = State()
-    waiting_for_portfolio = State()
-    waiting_for_source = State()
